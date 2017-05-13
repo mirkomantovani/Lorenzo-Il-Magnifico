f6ebac3f2e2365c4f6a13cc403f7ec978cb7b182
jimmyhypi <xhimi.hypi@mail.polimi.it>
2017-05-13 23:53:12 +0200

    ADDED: class ForEachTypeCardEffect

---
+++ src/main/java/it/polimi/ingsw/ps19/model/effect/ForEachTypeCardEffect.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.ps19.model.effect;
+
+public class ForEachTypeCardEffect extends Effect {
+
+	@Override
+	public void applyEffect() {
+		
+	}
+
+}
--- src/main/java/it/polimi/ingsw/ps19/model/effect/InstantResourcesEffect.java
+++ src/main/java/it/polimi/ingsw/ps19/model/effect/InstantResourcesEffect.java
@@ -1,9 +1,14 @@
 package it.polimi.ingsw.ps19.model.effect;
 
+import it.polimi.ingsw.ps19.ResourceChest;
+
 public class InstantResourcesEffect extends Effect {
-
+	
+	ResourceChest givenResourceChest;
+	
 	public void applyEffect() {
-
+	
+		
 	}
 
 }
