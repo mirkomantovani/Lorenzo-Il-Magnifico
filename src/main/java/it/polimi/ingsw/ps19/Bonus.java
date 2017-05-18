21309a17bb7e122baa972a2415181e0354e638df
MatteoMarziali <matteo.marziali@mail.polimi.it>
2017-05-18 23:10:35 +0200

    ADDED: Bonus

---
+++ src/main/java/it/polimi/ingsw/ps19/Bonus.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.ps19;
+
+public class Bonus {
+	
+	int harvestModification; //it means the increase/decrease of the Harvest action value for the specific player
+	int productionModification; // "  "  "  " 
+	
+	int takingBuildingCardBonus;
+	int takingCharacterCardBonus;
+	int takingVentureCardBonus;
+	int takingTerritoryBonus;
+
+}
