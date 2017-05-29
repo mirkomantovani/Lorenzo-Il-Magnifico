7bb5c8e0475bb9d4fda990a229e59e158461d4ef
mirkomantovani <mirko.mantovani@mail.polimi.it>
2017-05-30 00:01:31 +0200

    FIXED bug in ResourceMalusEffect, finished implementing ExcommunicationCard acquisition, implemented Church class

--- src/main/java/it/polimi/ingsw/ps19/model/area/Board.java
+++ src/main/java/it/polimi/ingsw/ps19/model/area/Board.java
@@ -35,7 +35,7 @@ public class Board {
 		towers.add(new Tower(CardType.CHARACTER, characterCards,bonuses));
 		towers.add(new Tower(CardType.VENTURE, ventureCards,bonuses));
 		
-//		church = new Church(null,null,null);
+		church = new Church();
 		councilPalace = new CouncilPalace();
 	//TODO	market = new Market(Match.getPlayers()); //this method should return the number of players in the match
 		harvestArea = new HarvestArea();
--- src/main/java/it/polimi/ingsw/ps19/model/card/CardConstants.java
+++ src/main/java/it/polimi/ingsw/ps19/model/card/CardConstants.java
@@ -11,13 +11,10 @@ public class CardConstants {
 		
 	}
 
-	
-	/** The Constant BUILDINGCARD_FILE_ENTRIES. */
-	public static final int BUILDINGCARD_FILE_ENTRIES=38;
-	
-	/** The Constant DECK_LENGTH. */
 
 	public static final int DECK_LENGTH=24;
 	public static final int MAX_PERSONAL_DECK_SIZE=6;
 	public static final int LEADER_DECK_LENGTH = 20;
+	
+	public static final int EXCOMMUNICATION_TILES=21;
 }
