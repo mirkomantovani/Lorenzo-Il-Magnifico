3371374328b33f5951914bb47597d17290dccaba
mirkomantovani <mirko.mantovani@mail.polimi.it>
2017-06-11 00:39:32 +0200

    ADDED: UserInterface with some methods, GraphicalUserInterface, started implementing CommandLineInterface

--- src/main/java/it/polimi/ingsw/ps19/view/UserInterface.java
+++ src/main/java/it/polimi/ingsw/ps19/view/UserInterface.java
@@ -1,5 +1,33 @@
 package it.polimi.ingsw.ps19.view;
 
+/**
+ * @author Mirko
+ *
+ */
 public interface UserInterface {
 
+	
+	void initializeMatch();
+	
+	void initializeTurn();
+	
+	void startTurn();
+	
+	void commandNotValid();
+	
+	void playerStatusChange();
+	
+	/**
+	 * Notification of a generic player move
+	 */
+	void playerMove();
+	
+	/**
+	 * It's <player> turn!
+	 */
+	void playerTurn();
+	
+	void win();
+	
+	void lose();
 }
