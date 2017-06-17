cc9234556ea35bb598f1e0c2c5960ba973e6c041
jimmyhypi <xhimi.hypi@mail.polimi.it>
2017-06-18 01:41:53 +0200

    ADDED privilege resources acquisition

--- src/main/java/it/polimi/ingsw/ps19/constant/FileConstants.java
+++ src/main/java/it/polimi/ingsw/ps19/constant/FileConstants.java
@@ -26,5 +26,5 @@ public class FileConstants {
 	public final static String VENTURECARDS = "src/main/resources/files/fileventurecards.txt";
 	public final static String LEADERCARDS = "src/main/resources/files/fileleadercards.txt";
 	public static final String EXCOMMUNICATIONTILES="src/main/resources/files/fileexcommunicationtiles.txt";
-	
+	public static final String PRIVILEGE_RESOURCES  = "src/main/resources/files/fileprivilegeresources.txt";
 }
