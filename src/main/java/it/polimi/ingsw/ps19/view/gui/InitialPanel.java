f97843d3ec82af1fd33b2eb23e6c5084ae72989a
mirkomantovani <mirko.mantovani@mail.polimi.it>
2017-06-21 11:58:23 +0200

    Scaled initial Image in GUI

--- src/main/java/it/polimi/ingsw/ps19/view/gui/InitialPanel.java
+++ src/main/java/it/polimi/ingsw/ps19/view/gui/InitialPanel.java
@@ -1,23 +1,52 @@
 package it.polimi.ingsw.ps19.view.gui;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.IOException;
 
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class InitialPanel extends JPanel {
 
 	
 	private static final long serialVersionUID = 1L;
+	private transient Toolkit toolkit = Toolkit.getDefaultToolkit();
+	private Dimension screenDimension;
+	private Image img;
 
-	public InitialPanel(ImageIcon image) {
-		setLayout(new BorderLayout());
-		setBackground(Color.BLACK);
-		JLabel bckgndImg = new JLabel(image, JLabel.CENTER);
-
-		this.add(bckgndImg, BorderLayout.CENTER);
+	public InitialPanel(String image) {
+		super(new GridBagLayout());
+		
+		screenDimension = toolkit.getScreenSize();
+		
+		try {
+			img = ImageIO.read(this.getClass().getResource(image));
+		} catch (IOException e) {
+		}
+		System.out.println(screenDimension.width+" e "+ screenDimension.height);
+		img = img.getScaledInstance(screenDimension.width, screenDimension.height,
+				Image.SCALE_SMOOTH);
+		screenDimension.setSize(screenDimension.width, screenDimension.height);
+		setPreferredSize(screenDimension);
+		
+		
 	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		g.drawImage(img, 0, 0, this);
+	}
+	
+//		setLayout(new BorderLayout());
+//		setBackground(Color.BLACK);
+//		JLabel bckgndImg = new JLabel(image, JLabel.CENTER);
+//
+//		this.add(bckgndImg, BorderLayout.CENTER);
+//	}
 }
 
--- src/main/java/it/polimi/ingsw/ps19/view/gui/MyFrame.java
+++ src/main/java/it/polimi/ingsw/ps19/view/gui/MyFrame.java
@@ -3,10 +3,14 @@ package it.polimi.ingsw.ps19.view.gui;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.Toolkit;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import it.polimi.ingsw.ps19.constant.ImagesConstants;
@@ -23,6 +27,7 @@ public class MyFrame extends JFrame {
 	private int height;
 	private int width;
 	private int toolBarHeight;
+	private Image img;
 
 	private Container content;
 //	private GameBoard board;
@@ -33,6 +38,7 @@ public class MyFrame extends JFrame {
 	public MyFrame() {
 		super("Lorenzo Il Magnifico");
 
+		setUndecorated(true);
 		setLayout(new BorderLayout());
 		content = this.getContentPane();
 
@@ -53,32 +59,34 @@ public class MyFrame extends JFrame {
 	}
 
 	private void addInitialPanel(String initialImage) {
-		ImageIcon backgroundImg = new ImageIcon(this.getClass().getResource(
-				initialImage));
 		
-		backgroundImg=backgroundImg.
-		Image a;		
-        Image image= backgroundImg.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
-		int imageWidth = backgroundImg.getIconWidth();
-		int imgHeight = backgroundImg.getIconHeight();
 		
-		initialPanel = new InitialPanel(backgroundImg);
-		initialPanel.setSize(new Dimension(imageWidth, imgHeight));
-		initialPanel.setPreferredSize(new Dimension(imageWidth, imgHeight));
-		content.add(initialPanel);
-		pack();
-		setVisible(true);
-		setResizable(false);
-		setLocationRelativeTo(null);	
+//		ImageIcon backgroundImg = new ImageIcon(this.getClass().getResource(
+//				initialImage));
+//		
+//		int imageWidth = backgroundImg.getIconWidth();
+//		int imgHeight = backgroundImg.getIconHeight();
+//		
+		initialPanel = new InitialPanel(initialImage);
 		
-		Runnable repaintFrame = new Runnable() {
-			@Override
-			public void run(){
-				content.repaint();
-			}
-		};
-		
-		SwingUtilities.invokeLater(repaintFrame);
+		content.add(initialPanel, BorderLayout.CENTER);
+		initialPanel.setLayout(null);
+//		initialPanel.setSize(new Dimension(imageWidth, imgHeight));
+//		initialPanel.setPreferredSize(new Dimension(imageWidth, imgHeight));
+//		content.add(initialPanel);
+//		pack();
+//		setVisible(true);
+//		setResizable(false);
+//		setLocationRelativeTo(null);	
+//		
+//		Runnable repaintFrame = new Runnable() {
+//			@Override
+//			public void run(){
+//				content.repaint();
+//			}
+//		};
+//		
+//		SwingUtilities.invokeLater(repaintFrame);
 	}	
 
 }
